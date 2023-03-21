e94a79f2cd1bb483de27f33da9797539ad438865
Darrell <ashilpadarrel649@gmail.com>
2023-03-21 21:07:06 +0700

    Chapter 14 Equals

---
+++ Chapter14/Ch14Equals.java
@@ -0,0 +1,56 @@
+package Chapter14;
+
+import java.util.Stack;
+
+public class Ch14Equals{
+    public static void main(String[] args) {
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+        Stack<Integer> stack3 = new Stack<Integer>();
+
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+        stack1.push(4);
+
+        stack2.push(1);
+        stack2.push(2);
+        stack2.push(3);
+        stack2.push(4);
+
+        stack3.push(1);
+        stack3.push(2);
+        stack3.push(2);
+        stack3.push(4);
+
+        System.out.println("Stack 1 Sebelum : " + stack1);
+        System.out.println("Stack 2 Sebelum : " + stack2);
+        System.out.println("Stack 3 Sebelum : " + stack3);
+
+        System.out.println();
+
+        System.out.println("Equals.(stack1, stack2) : " + Equals(stack1, stack2));
+        System.out.println("Equals.(stack1, stack3) : " + Equals(stack1, stack3));
+
+        System.out.println();
+
+        System.out.println("Stack 1 Setelah : " + stack1);
+        System.out.println("Stack 2 Setelah : " + stack2);
+        System.out.println("Stack 3 Setelah : " + stack3);
+    }
+
+    public static boolean Equals(Stack<Integer> stack1, Stack<Integer> stack2){
+        Stack<Integer> sementara1 = new Stack<Integer>(), sementara2 = new Stack<Integer>();
+
+        sementara1.addAll(stack1);
+        sementara2.addAll(stack2);
+
+        while(!sementara1.isEmpty()){
+            if(sementara1.pop() != sementara2.pop()){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
